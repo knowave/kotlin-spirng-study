be329d530f5526bc6a8a79449383ccc580571ebb
knowave <kwaktaemin98@gmail.com>
2025-06-05 18:09:49 +0900

    dotenv 적용 및 findAll method 제거

--- src/main/kotlin/com/example/board/Application.kt
+++ src/main/kotlin/com/example/board/Application.kt
@@ -1,5 +1,6 @@
 package com.example.board
 
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,11 @@ import org.springframework.boot.runApplication
 class Application
 
 fun main(args: Array<String>) {
+    val dotenv = dotenv()
+
+    dotenv.entries().forEach { entry ->
+        System.setProperty(entry.key, entry.value)
+    }
+
     runApplication<Application>(*args)
 }
--- src/main/kotlin/com/example/board/post/PostRepository.kt
+++ src/main/kotlin/com/example/board/post/PostRepository.kt
@@ -3,6 +3,4 @@ package com.example.board.post.repository
 import com.example.board.post.entity.Post
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PostRepository : JpaRepository<Post, Long> {
-    fun findByAll(): List<Post>
-}
+interface PostRepository : JpaRepository<Post, Long>
