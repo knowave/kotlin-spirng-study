986abb2d4069571af13ba238132844d13cf55020
knowave <kwaktaemin98@gmail.com>
2025-06-11 11:52:00 +0900

    Configuration Spring Security

--- kotlin-spring-security/src/main/kotlin/com/example/kotlin_spring_security/common/config/SecurityConfig.kt
+++ kotlin-spring-security/src/main/kotlin/com/example/kotlin_spring_security/common/config/SecurityConfig.kt
@@ -2,14 +2,32 @@ package com.example.kotlin_spring_security.common.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class PasswordEncoderConfig {
+@EnableWebSecurity
+class SecurityConfig {
 
     @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/api/member/signup", "/api/memer/sign-in").anonymous()
+                    .anyRequest().permitAll()
+            }
+
+        return http.build()
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder =
+        BCryptPasswordEncoder()
 }
