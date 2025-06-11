4619d9ba355b8cdc65ac2f077483c6909538e36e
knowave <kwaktaemin98@gmail.com>
2025-06-11 11:53:25 +0900

    member의 password encoding 처리

--- kotlin-spring-security/src/main/kotlin/com/example/kotlin_spring_security/member/service/MemberService.kt
+++ kotlin-spring-security/src/main/kotlin/com/example/kotlin_spring_security/member/service/MemberService.kt
@@ -5,12 +5,14 @@ import com.example.kotlin_spring_security.member.dto.MemberDtoRequest
 import com.example.kotlin_spring_security.member.entity.Member
 import com.example.kotlin_spring_security.member.repository.MemberRepository
 import jakarta.transaction.Transactional
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Transactional
 @Service
 class MemberService(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: PasswordEncoder
 ) {
     /**
      * 회원가입
@@ -23,7 +25,8 @@ class MemberService(
             throw InvalidInputException("loginId", "이미 등록된 ID 입니다.")
         }
 
-        member = memberDtoRequest.toEntity()
+        val encodePassword = passwordEncoder.encode(memberDtoRequest.password)
+        member = memberDtoRequest.copy(_password = encodePassword).toEntity()
         memberRepository.save(member)
 
         return "회원가입이 완료되었습니다."
