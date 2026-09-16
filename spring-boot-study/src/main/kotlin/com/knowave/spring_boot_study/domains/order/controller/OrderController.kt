86b336622421c6a5e26fcef2b8f515be09452c00
knowave <kwaktaemin98@gmail.com>
2026-09-16 17:42:19 +0900

    FIX: Controller 어노테이션 수정

--- spring-boot-study/src/main/kotlin/com/knowave/spring_boot_study/domains/order/controller/OrderController.kt
+++ spring-boot-study/src/main/kotlin/com/knowave/spring_boot_study/domains/order/controller/OrderController.kt
@@ -6,12 +6,12 @@ import com.knowave.spring_boot_study.domains.order.service.OrderService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller("/api")
+@RestController("/api")
 class OrderController(
     private val orderService: OrderService
 ) {
